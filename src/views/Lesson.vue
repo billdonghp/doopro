8098fbd602b20b0c2b88b9b0cd8c9344977dc74b
DOOSAN AppleMacBook Pro <doosanapplemacbookpro@DOOSANdeMacBook-Pro.local>
2020-08-11 08:56:11 +0800

    20200811

---
+++ src/views/Lesson.vue
@@ -0,0 +1,5 @@
+<template>
+   <div id="lesson">
+       <el-image url="../assets/1.jpg"></el-image>
+     </div>   
+</template>
--- src/views/User.vue
+++ src/views/User.vue
@@ -1,5 +1,8 @@
 <template>
   <div id="user">
+      <div class="img1"></div>
+      <div class="img2"></div>
+      <div class="img3"></div>
     <div>User {{ username }}</div>
     <el-button type="primary" @click="goinfo">充值</el-button>
   </div>
@@ -9,6 +12,12 @@
 export default {
   name: 'User',
   props: ['username'],
+  data () {
+    return {
+      fit: 'fill',
+      url: './assets/3.png'
+    }
+  },
   methods: {
     goinfo () {
       // this.$router.go(-1)
@@ -18,9 +27,22 @@ export default {
 }
 </script>
 <style>
-  #user{
-    width: 200px;
-    height: 300px;
-    margin: 0 auto;
-  }
+.img1{
+  width:150px;
+  height: 180px;
+  background: url(../assets/1.jpg) center no-repeat;
+  background-size: 100%;
+}
+.img2{
+  width:150px;
+  height: 180px;
+  background: url(../assets/2.jpg) center no-repeat;
+  background-size: 100%;
+}
+.img3{
+  width:150px;
+  height: 180px;
+  background: url(../assets/3.png) center no-repeat;
+  background-size: 100%;
+}
 </style>
